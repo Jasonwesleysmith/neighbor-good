8e6d21be3fedca5aab19234e0dfd960c5996ef3b
Tez28 <courtezcannady@gmail.com>
2022-05-18 15:29:01 -0500

    dashboard and supporting divs

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -4,7 +4,13 @@ function Dashboard() {
   
     return (
         <div>
-            
+            <h1>Dashboard</h1>
+            <div>
+                <h2>New Job</h2>
+            </div>
+            <div>
+                <h2>All Jobs</h2>
+            </div>
         </div>
     )
 
