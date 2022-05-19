eb9fcf2323d4422d8f8d2df2e2f69d2d05d4fcfd
Tez28 <courtezcannady@gmail.com>
2022-05-19 12:19:15 -0400

    input header function

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,3 +3,21 @@ import Dashboard from "../Dashboard";
 import Claimed from "../Claimed";
 import Unclaimed from "../Unclaimed"
 import Nav from "../Nav";
+
+function Header () {
+    const [currentPage, handlePageChange] = useState("Dashboard");
+    const renderPage = () => {
+        switch (currentPage) {
+            case "Dashboard":
+                return <Dashboard/>;
+            case "Claimed":
+                return <Claimed/>;
+            case "Unclaimed":
+                return <Unclaimed/>;
+            default:
+                return <Dashboard/>;
+        }
+    };
+}
+
+export default Header;
