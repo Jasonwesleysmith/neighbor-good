ab8a60891d1081f51b75c7441170ee3e42444c95
Tez28 <courtezcannady@gmail.com>
2022-05-19 12:43:11 -0400

    add footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <p>
+                    <strong>© NeighborGood</strong>
+                    <hr/>
+                    <a>
+                        NeighborGood is a service webpage dedicated to finding "Good Neighbors" to help their neighbors
+                    </a>
+                </p>
+            </div>
+        </footer>
+    )
+}
