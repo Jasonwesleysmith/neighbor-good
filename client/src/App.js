4542f22a2445fb0168c5b70adde70ba4723d3eb4
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-05-20 00:48:32 -0500

    styled component styling & job component button development

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 import Modal from './components/Modal'
 import UnclaimedJobs from './components/UnclaimedJobs/UnclaimedJobs';
 
 const jobs = [
   {id: 0, claimed: false, claimedBy: null, job:"Drummer", pay:"5", name:"Ben", phone:"5555555", location:"Grad party", description:"Party Music", info:""},
-  {id: 1, claimed: false, claimedBy: null, job:"Lawn Mowed", pay:"20", name:"Alice", phone:"5554444", location:"Main St", description:"Mow lawn", info:"non rainy days"},
+  {id: 1, claimed: true, claimedBy: null, job:"Lawn Mowed", pay:"20", name:"Alice", phone:"5554444", location:"Main St", description:"Mow lawn", info:"non rainy days"},
   {id: 2, claimed: false, claimedBy: null, job:"Coffee Table moved", pay:"5", name:"Joe", phone:"5553333", location:"Joes house", description:"move my coffee table", info:"move downstairs"},
 ]
 
