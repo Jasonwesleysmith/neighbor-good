221ad19f7898ea887285db5b09bf5e6622d8542f
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-05-19 21:51:28 -0500

    added component files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import ClaimedJob from './components/ClaimedJob';
+import UnclaimedJob from './components/UnclaimedJob';
+import Modal from './components/Modal'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ClaimedJob />
+      <UnclaimedJob />
+      <Modal>
+        this is where the children are
+
+
+      </Modal>
+    </>
   );
 }
 
---
+++ client/src/components/UnclaimedJob/UnclaimedJob.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UnclaimedJob = () => {
+  return (
+    <div>UnclaimedJob</div>
+  )
+}
+
+export default UnclaimedJob
