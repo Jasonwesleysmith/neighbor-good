2424d48e72eeb3fd5acbef4c224ad36fb6ceabf5
Tez28 <courtezcannady@gmail.com>
2022-05-21 11:50:25 -0400

    import react to nav

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -7,9 +7,15 @@ function Dashboard() {
             <h1>Dashboard</h1>
             <div>
                 <h2>New Job</h2>
+                <a className="button" href="../Job" target="_blank" rel="noreferrer">
+                    <span>New Job</span>
+                </a>
             </div>
             <div>
                 <h2>All Jobs</h2>
+                <a className="button" href="../UnclaimedJobs" target="_blank" rel="noreferrer">
+                    <span>Open Jobs</span>
+                </a>
             </div>
         </div>
     )
