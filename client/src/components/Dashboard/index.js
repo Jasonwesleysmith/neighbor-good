8ed7964f2fe311e55c9b324686e0f092cfd15fab
Tez28 <courtezcannady@gmail.com>
2022-05-21 15:13:00 -0400

    restyle dashboard

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -4,61 +4,48 @@ import styled from 'styled-components'
 function Dashboard() {
   
     return (
-        <>
-            <DashboardStyled>
-                <h1>Dashboard</h1>
-            </DashboardStyled>
+        <DashboardStyled>
+            <h1 className="title">Dashboard</h1>
             <div>
-                <TitleStyled>
-                    <h2>New Job</h2>
-                </TitleStyled>
-                <ButtonStyled>
-                    <a className="button" href="../Job" target="_blank" rel="noreferrer">
-                        <h3>New Job</h3>
-                    </a>
-                </ButtonStyled>
+                <h2>New Job</h2>
+                <a className="button" href="../Job" target="_blank" rel="noreferrer">
+                    <span>New Job</span>
+                </a>
             </div>
             <div>
-                <TitleStyled>
-                    <h2>All Jobs</h2>
-                </TitleStyled>
-                <ButtonStyled>
-                    <a className="button" href="../UnclaimedJobs" target="_blank" rel="noreferrer">
-                        <h3>Open Jobs</h3>
-                    </a>
-                </ButtonStyled>
+                <h2 className="title">All Jobs</h2>
+                <a className="button" href="../UnclaimedJobs" target="_blank" rel="noreferrer">
+                    <span>Open Jobs</span>
+                </a>
             </div>
-        </>
+        </DashboardStyled>
     )
 
 }
 
 const DashboardStyled = styled.section `
-    font-size: 2em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border-radius: 3px;
-    text-align: center;
-`
-const ButtonStyled = styled.section `
-    font-size: .75em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border-radius: 3px;
-    text-align: center;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  text-align: center;
 
-    /* Color the border and text with theme.main */
-    color: ${props => props.theme.main};
-    border: 2px solid ${props => props.theme.main};
-`;
+  h1{
+    font-size: 2.5em;
 
-const TitleStyled = styled.section `
+  }
+  .title {
+    font-size: 1.5em;
+  }
+
+  .button{
     font-size: 1em;
     margin: 1em;
     padding: 0.25em 1em;
     border-radius: 3px;
-    text-align: center;
-
-`
+  
+    /* Color the border and text with theme.main */
+    color: ${props => props.theme.main};
+    border: 2px solid ${props => props.theme.main};
+`;
 
 export default Dashboard;
